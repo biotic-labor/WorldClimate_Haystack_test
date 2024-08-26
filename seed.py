912aa018f480d151a8eed65005aa32d34e0fe358
Chris Nelson <dj.ccelson@gmail.com>
2024-08-26 15:07:45 -0500

    Removing unused lines of code

--- seed.py
+++ seed.py
@@ -1,9 +1,6 @@
 import climate_data_summarizer as cds   
 import webapp_data_writer as wd
 
-location = {"city_name":"Mumbai", "country":"India"}
-query_type = "num_days_above_90"
-
 data = {
     "locations":[
     {
